9e0fbb24177146bec08ce921e0ea920a5b22f944
neho <alexneho@yahoo.com>
2026-04-20 12:08:10 +0300

    Added drag&drop functionality to the review page
    This allows the user to reorder attractions by dragging and dropping them. The attraction can either be moved within the same day or between two different days

--- src/Review.jsx
+++ src/Review.jsx
@@ -1,39 +1,97 @@
 import { useState } from 'react'
 import './Review.css'
 
-function Day({name}) {
-    let attractions = ["cool", "super-cool", "mega-cool"];
+function Day({dayIndex, attractions, onDragStart, onDragOver, onDrop}) {
     return (
         <section class = "day-card">
-            <h2> Day {name} ! </h2>
-            <ul class="attractions-container">
-                { attractions.map ( (attraction) => {
-                    return <Attraction 
-                        name={attraction} description="desc" 
+            <h2> Day {dayIndex + 1} ! </h2>
+            <ol class="attractions-container">
+                { attractions.map ( (attraction, index) => {
+                    return <Attraction // dont forget to add keys to each attraction
+                        name={attraction}
+                        index={index}
+                        dayIndex={dayIndex}
+                        onDragStart={onDragStart}
+                        onDragOver={onDragOver}
+                        onDrop={onDrop}
+                        description="desc" 
                     />
                 } )}
-            </ul>
+            </ol>
         </section>
     )
 }
 
-function Attraction(props) {
+function Attraction({name, index, dayIndex, onDragStart, onDragOver, onDrop, description}) {
     return (
-        <li>
-            <div class="attraction-title"> Attraction {props.name} !</div>
-            <p> Description {props.description} </p>
+        <li
+        draggable="true"
+        onDragStart={(e) => onDragStart(e, index, dayIndex)}
+        onDragOver={(e) => onDragOver(e)}
+        onDrop={(e) => onDrop(e, index, dayIndex)}
+        >
+            <div class="attraction-title"> Attraction {name} !</div>
+            <p> Description {description} </p>
         </li>
     )
 }
 
 function Review() {
-    let days = [1, 2, 3];
+    // this receives a list of attractions and restaurants
+    // and it has to split it between multiple days
+
+    const [attractions, setAttractions] = useState([
+        ["cool", "super-cool", "mega-cool"],
+        ["ultra-cool", "immensely-cool"]
+    ]);
+    
+    const [draggedItem, setDraggedItem] = useState(null);
+
+    const handleDragStart = (e, index, dayIndex) => {
+        // store the data of the dragged item
+        const data = {
+            attractionIndex: index,
+            dayIndex: dayIndex
+        };
+        setDraggedItem(data)
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const handleDrop = (e, targetIndex, targetDayIndex) => {
+        e.preventDefault();
+
+        // create a copy of the list of attractions for the target day
+        const newList = [...attractions];
+
+        console.log(targetDayIndex);
+        console.log(draggedItem['dayIndex']);
+        
+        // remove the selected attraction from the initial position
+        const removedAttraction = newList[draggedItem['dayIndex']].splice(draggedItem['attractionIndex'], 1);
+
+        // add it to the new position
+        newList[targetDayIndex].splice(targetIndex, 0, removedAttraction[0]);
+
+        setAttractions(newList);
+        setDraggedItem(null);
+    };
+
     return (
         <main>
             <h1>Review Trip Details</h1>
             <div class="days-container">
-                { days.map( (day) => {
-                    return <Day key={day} name={day} />
+                { attractions.map( (day, index) => {
+                    return <Day
+                        key={day}
+                        dayIndex={index}
+                        attractions={day}
+                        onDragStart={handleDragStart}
+                        onDragOver={handleDragOver}
+                        onDrop={handleDrop}
+                    />
                 }) }
             </div>
         </main>
